59e9dc14f313652ab468f6ccb9deea9b927febd2
Madis Pink <madis.pink@gmail.com>
2014-03-22 20:22:22 +0100

    Add more information about the bug

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 android-appcompat-bug
 =====================
 
-Reproduce a bug in appcompat library where fragments get onCreateOptionsMenu callbacks in a detached state
+A small toy app to demonstrate a bug in appcompat which may result in a crash.
+
+If a fragment with options menu items is added to the activity then in some
+cases the fragment may receive `onCreateOptionsMenu` callbacks whilst the
+fragment is detached. This will often lead to a crash as it is quite common to
+load resources in the callback - for instance to set a title for a menu option.
+
+This will yield an `IllegalStateException` from `Fragment.getResources()` as the
+fragment isn't attached any more. This happens iff your app happens to call the
+`supportInvalidateOptionsMenu()` method.
+
+The specifics:
+
+* When `ActionBarActivity.supportInvalidateOptionsMenu` is called it posts an
+  invalidate runnable to DecorView (method
+  `ActionBarActivityDelegateBase.supportInvalidateOptionsMenu`)
+* This runnable will make FragmentActivity (super of ActionBarActivity) dispatch
+  the `onCreateOptionsMenu` callbacks on all the fragments. Note that this is
+  usually the desired functionality...
+
+  *...unless...*
+
+* ...due to some reason, say a configuration change, our activity instance was
+  destroyed before the posted runnable had a chance to run. In this case the
+  `onCreateOptionsMenu` callbacks will still be dispatched and the app will
+  crash.
+
+This toy app exhibits the described case.
--- app/src/main/java/com/madisp/fragmentsbroken/CrashActivity.java
+++ app/src/main/java/com/madisp/fragmentsbroken/CrashActivity.java
@@ -5,18 +5,49 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
+/**
+ * A small toy app to demonstrate a bug in appcompat which may result in a crash.
+ *
+ * If a fragment with options menu items is added to the activity then in some cases the fragment
+ * may receive onCreateOptionsMenu callbacks whilst the fragment is detached. This will often lead
+ * to a crash as it is quite common to load resources in the callback - for instance to set a title
+ * for a menu option. This will yield an IllegalStateException from Fragment.getResources() as the
+ * fragment isn't attached any more. This happens iff your app happens to call the
+ * supportInvalidateOptionsMenu() method.
+ *
+ * The specifics:
+ *
+ * 1) When ActionBarActivity.supportInvalidateOptionsMenu is called it posts an invalidate runnable
+ *    to DecorView (method ActionBarActivityDelegateBase.supportInvalidateOptionsMenu)
+ * 2) This runnable will make FragmentActivity (super of ActionBarActivity) dispatch the
+ *    onCreateOptionsMenu callbacks on all the fragments. Note that this is usually the desired
+ *    functionality...
+ *
+ *    *unless*
+ *
+ * 3) Due to some reason, say a configuration change, our activity instance was destroyed before the
+ *    posted runnable had a chance to run. In this case the onCreateOptionsMenu callbacks will still
+ *    be dispatched and the app will crash.
+ *
+ * This class exhibits the described case.
+ */
 public class CrashActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// force to portrait so we can easily trigger a conf change later (orienting to landscape)
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		// just a framelayout with id=@id/main
 		setContentView(R.layout.main);
+		// add a standard fragment with a few views and a menu option
 		getSupportFragmentManager().beginTransaction().add(R.id.main, new CrashFragment()).commit();
 	}
 
 	public void crash(View v) {
+		// lets say we need to invalidate our options menu
 		supportInvalidateOptionsMenu();
 		// A wild configuration change appeared!
+		// Configuration change used crash. It's super effective!
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 	}
 }
